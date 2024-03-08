6055c167033367ba0e69db8541f9ba4b249b9e0a
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-03-08 17:48:19 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) { }
 
 .onAttach <- function(libname, pkg) {
-  intro <- "adatutoR - Introducing the Boosting Framework with AdaBoost"
+  intro <- "adatutoR: Introducing the Boosting Framework with AdaBoost"
   version <- packageVersion(pkg)
   packageStartupMessage(paste0(intro, "\n", "Version: ", version))
 }
