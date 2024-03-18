7087390ff945b99d7bacce9e46ba7bda41a9f335
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-03-18 18:14:23 +0100

    Update altmext723.R
    Make the outcome variable a factor and fix row names

--- data-raw/altmext723.R
+++ data-raw/altmext723.R
@@ -16,5 +16,15 @@ voi <- c("eid", "pid", "power.o", "effect_size.o", "n.o", "p_value.o",
 altmext723 <- rbind.data.frame( altmejd723[,colnames(altmejd723) %in% voi],
                                 ssrp723[,colnames(ssrp723) %in% voi] )
 
+# Make the outcome a factor and put it on the 1/2 scale (instead of 0/1)
+altmext723$replicated <- factor(altmext723$replicated + 1, # rpart consistency
+                                labels = c("no", "yes"))
+
+# Sort the data set by eid
+altmext723 <- altmext723[order(altmext723$eid),]
+
+# Add rownames
+rownames(altmext723) <- paste0(seq(nrow(altmext723)))
+
 # Save the data set as R data
 usethis::use_data(altmext723, overwrite = TRUE)
