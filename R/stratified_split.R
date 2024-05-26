98d49f9baf92eec2f8f36f86003ff7aefcb8e637
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-05-26 11:34:07 +0200

    Create stratified_split.R

---
+++ R/stratified_split.R
@@ -0,0 +1,11 @@
+#'@export
+stratified_split <- function(set, group, id, sprop = 0.7, seed = 112) {
+  set <- as.data.frame(set) ; group <- as.character(group)
+  id <- as.character(id) ; set.seed(seed)
+  set_split <- split(set, set[,group])
+  n_set <- lapply(set_split, function(set) round(nrow(set) * sprop))
+  strat_idx <- mapply(function(set, n) sample(set[,id], n),
+                      set_split, n_set)
+  set_idx <- set[,id] %in% unlist(strat_idx)
+  list("set1" = set[set_idx, ], "set2" = set[!set_idx, ])
+}
