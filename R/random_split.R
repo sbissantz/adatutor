91e187c0a8b1ed1fcd6b88a96f4cf4b54330df3b
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-06-13 18:07:30 +0200

    Change argument name

--- R/random_split.R
+++ R/random_split.R
@@ -1,8 +1,8 @@
 #'@export
-random_split <- function(set, sprop = 0.7, seed = 112) {
+random_split <- function(set, prop = 0.7, seed = 112) {
   set <- as.data.frame(set) ; set.seed(seed)
   n_effect <- nrow(set)
-  n_split <- round(n_effect * sprop)
+  n_split <- round(n_effect * prop)
   split_idx <- sample(seq_len(n_effect), n_split)
   # Training and testing set
   list("set1" = set[split_idx,], "set2" = set[-split_idx,] )
