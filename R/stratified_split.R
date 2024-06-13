373f9abbb7495df07abf84ebfcf0361d96761b5c
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-06-13 18:07:39 +0200

    Change argument name

--- R/stratified_split.R
+++ R/stratified_split.R
@@ -1,9 +1,9 @@
 #'@export
-stratified_split <- function(set, group, id, sprop = 0.7, seed = 112) {
+stratified_split <- function(set, group, id, prop = 0.7, seed = 112) {
   set <- as.data.frame(set) ; group <- as.character(group)
   id <- as.character(id) ; set.seed(seed)
   set_split <- split(set, set[,group])
-  n_set <- lapply(set_split, function(set) round(nrow(set) * sprop))
+  n_set <- lapply(set_split, function(set) round(nrow(set) * prop))
   strat_idx <- mapply(function(set, n) sample(set[,id], n),
                       set_split, n_set)
   set_idx <- set[,id] %in% unlist(strat_idx)
