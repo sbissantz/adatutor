e4aac9b28a7b6c882fac2b26c4673c19866495c1
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-09-11 16:54:06 +0200

    Create test_adaboost2.R

---
+++ R/test_adaboost2.R
@@ -0,0 +1,26 @@
+#'@export
+test_adaboost2 <- function(H, data, input_checks = TRUE) {
+  message("Start the AdaBoost test process:")
+  if(isTRUE(input_checks)) {
+    message("Run mild input checks.")
+    check_list(H) ; check_empty(H)
+    check_list(data) ; check_empty(data)
+  }
+  message("Step 1: Extract the trees.")
+  h <- lapply(H, "[[", "h") ; check_empty(h)
+  message("Step 2: Extract the model weights.")
+  a <- vapply(H, "[[", FUN.VALUE = numeric(1), "a")
+  message("Step 3: Making predictions/retrodictions.")
+  pb <- txtProgressBar(min = 0, max = length(h), style = 3)
+  y12_stumps <- sapply(seq_along(h), function(i) {
+    setTxtProgressBar(pb, i)
+    predict(h[[i]], newdata = data, type = "vector")
+  })
+  close(pb)  # Close progress bar when done
+  message("Step 4: Combine predictions/retrodictions.")
+  ypred_stumps <- 2 * y12_stumps - 3
+  ypred_ada <- sign(a %*% t(ypred_stumps))
+  # Completion message
+  message("Test process successfully completed.\n")
+  ypred_ada
+}
