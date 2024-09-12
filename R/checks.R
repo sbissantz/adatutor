37d415a9d80f429ff9182d1a197253549e115a63
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-09-12 15:08:20 +0200

    Update checks.R

--- R/checks.R
+++ R/checks.R
@@ -1,26 +1,50 @@
-check_list <- function(x) {
-  if (!is.list(x)) {
-    x_inp <- substitute(x)
-    msg <- paste0("`", deparse(x_inp),"` is not a list. ")
-    sug <- paste0("Check if", "`", deparse(x_inp),"` is valid.")
-    warning(c(msg, sug), call. = FALSE)
-   }
-}
-
 check_df <- function(x) {
   if (!is.data.frame(x)) {
     x_inp <- substitute(x)
     msg <- paste0("`", deparse(x_inp),"` is not a data frame. ")
     sug <- paste0("Check if", "`", deparse(x_inp),"` is valid.")
-    warning(c(msg, sug), call. = FALSE)
+    stop(c(msg, sug), call. = FALSE)
    }
 }
 
-check_empty <- function(x) {
+check_eta <- function(x) {
+  x_inp <- substitute(x)
+  if (!is.numeric(x)) {
+    msg <- paste0("`", deparse(x_inp),"` is not a numeric value. ")
+    sug <- paste0("Check if", "`", deparse(x_inp),"` is valid.")
+    stop(c(msg, sug), call. = FALSE)
+  } else {
+    if (x > 1) {
+    msg <- paste0("The value of `eta` is greater than 1. ")
+    sug <- paste0("Think about overfitting.")
+    message(c(msg, sug))
+    }
+  }
+}
+
+check_length <- function(x) {
   if (length(x) == 0) {
     x_inp <- substitute(x)
     msg <- paste0("`", deparse(x_inp),"` has no elements. ")
     sug <- paste0("Check if", "`", deparse(x_inp),"` is valid.")
-    warning(c(msg, sug), call. = FALSE)
+    stop(c(msg, sug), call. = FALSE)
+   }
+}
+
+check_list <- function(x) {
+  if (!is.list(x)) {
+    x_inp <- substitute(x)
+    msg <- paste0("`", deparse(x_inp),"` is not a list. ")
+    sug <- paste0("Check if", "`", deparse(x_inp),"` is valid.")
+    stop(c(msg, sug), call. = FALSE)
+   }
+}
+
+check_numeric <- function(x) {
+  if (!is.numeric(x)) {
+    x_inp <- substitute(x)
+    msg <- paste0("`", deparse(x_inp),"` is not a numeric value. ")
+    sug <- paste0("Check if", "`", deparse(x_inp),"` is valid.")
+    stop(c(msg, sug), call. = FALSE)
    }
 }
