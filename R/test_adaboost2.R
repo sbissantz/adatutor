b5ad1f2396e07796bfe63a3fb639c41d00b7ac5c
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-09-12 15:08:10 +0200

    Update test_adaboost2.R

--- R/test_adaboost2.R
+++ R/test_adaboost2.R
@@ -4,18 +4,18 @@ test_adaboost2 <- function(H, data, input_checks = TRUE) {
   message("Start the AdaBoost test process:")
   if(isTRUE(input_checks)) {
     message("Run mild input checks.")
-    check_list(H) ; check_empty(H)
-    check_list(data) ; check_empty(data)
+    check_list(H) ; check_length(H)
+    check_df(data) ; check_length(data)
   }
   message("Step 1: Extract the trees.")
-  h <- lapply(H, "[[", "h") ; check_empty(h)
+  h <- lapply(H, "[[", "h") ; check_length(h)
   message("Step 2: Extract the model weights.")
-  a <- vapply(H, "[[", FUN.VALUE = numeric(1), "a")
+  a <- vapply(H, "[[", numeric(1), "a") ; check_length(a)
   message("Step 3: Make predictions/retrodictions.")
   pb <- txtProgressBar(min = 0, max = length(h), style = 3)
-  y12_stumps <- sapply(seq_along(h), function(i) {
-    setTxtProgressBar(pb, i)
-    predict(h[[i]], newdata = data, type = "vector")
+  y12_stumps <- sapply(seq_along(h), function(t) {
+    setTxtProgressBar(pb, t)
+    predict(h[[t]], newdata = data, type = "vector")
   })
   close(pb)  # Close progress bar when done
   message("Step 4: Combine predictions/retrodictions.")
