bf3af9a3375027fe76fbdf70c71b6dba909231e3
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-09-12 15:08:12 +0200

    Update train_adaboost2.R

--- R/train_adaboost2.R
+++ R/train_adaboost2.R
@@ -1,23 +1,33 @@
 #'@export
-train_adaboost2 <- function(fml, data, T, eta, hyptree) {
+train_adaboost2 <- function(fml, data, T, eta, tree_hyperpar, input_checks = TRUE) {
+  message("Start the AdaBoost training process:")
+  if(isTRUE(input_checks)) {
+    message("Run mild input checks.")
+    check_df(data) ; check_length(data)
+    check_eta(eta) ; check_numeric(T)
+  }
+  message("Start the initialization process.")
   H <- vector("list", T)
   m <- nrow(data)
   D <- rep(1, m)/m
   y_nme <- all.vars(as.formula(fml))[1]
   y_train <- data[[y_nme]]
+  message("Steps 1-4: Run through the algorithm steps.")
+  pb <- txtProgressBar(min = 0, max = T, style = 3)
   for (t in seq_len(T)) {
-    h <- rpart::rpart(formula = replicated ~ ., data = data ,
-                      method = "class", weights = D, control = hyptree)
-    y_retro <- predict(object = h, data, type = "class")
+    h <- rpart::rpart(as.formula(fml), data, D, method = "class",
+                      control = tree_hyperpar)
+    y_retro <- predict(h, data, type = "class")
     e <- sum(D * (y_train != y_retro))
-    a <- 0.5 * log( (1 - e)  / e) * eta
+    a <- 0.5 * log((1 - e) / e) * eta
     chi <- (y_train == y_retro) * 1 + (y_train != y_retro) * -1
     D_unorm <- (D * exp(-a * chi))
-    D <- D_unorm/sum(D_unorm)
-    # H[[t]] <- list("h" = h, "a" = a, "D" = D, "chi" = chi, "y_retro" =
-    # as.numeric(y_retro))
+    D <- D_unorm / sum(D_unorm)
     H[[t]] <- list("h" = h, "a" = a)
+    setTxtProgressBar(pb, t)
   }
+  close(pb)
+  message("Training process successfully completed.\n")
   names(H) <- paste0("t", seq_len(T))
   H
 }
