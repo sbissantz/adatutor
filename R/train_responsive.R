6c3190f774056339f01c29d9b64d6e9de11d003e
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-09-12 15:08:18 +0200

    Create train_responsive.R

---
+++ R/train_responsive.R
@@ -0,0 +1,25 @@
+train_responsive <- function(fml, data, T, eta, tree_hyperpar) {
+  message("Start the initialization process.")
+  H <- vector("list", T)
+  m <- nrow(data)
+  D <- rep(1, m)/m
+  y_nme <- all.vars(as.formula(fml))[1]
+  y_train <- data[[y_nme]]
+  message("Steps 1-4: Run through the algorithm steps.")
+  pb <- txtProgressBar(min = 0, max = T, style = 3)
+  for (t in seq_len(T)) {
+    h <- rpart::rpart(fml, data, D, method = "class",  control = tree_hyperpar)
+    y_retro <- predict(h, data, type = "class")
+    e <- sum(D * (y_train != y_retro))
+    a <- 0.5 * log((1 - e) / e) * eta
+    chi <- (y_train == y_retro) * 1 + (y_train != y_retro) * -1
+    D_unorm <- (D * exp(-a * chi))
+    D <- D_unorm / sum(D_unorm)
+    H[[t]] <- list("h" = h, "a" = a)
+    setTxtProgressBar(pb, t)
+  }
+  close(pb)
+  message("Training process successfully completed.\n")
+  names(H) <- paste0("t", seq_len(T))
+  H
+}
