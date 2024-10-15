de4d7ee9d60c52edf95dedf9efd1457f6aae0eb2
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-10-15 18:42:46 +0200

    Optimize function output

--- R/test_adaboost2.R
+++ R/test_adaboost2.R
@@ -2,11 +2,12 @@
 test_adaboost2 <- function(H, data, input_checks = TRUE) {
   message("Start the AdaBoost test process:")
   if(isTRUE(input_checks)) {
-    message("Run mild input checks.")
+    message("Run mild input checks.", appendLF = FALSE)
     check_list(H)
     check_length(H)
     check_df(data)
     check_length(data)
+    walking_dots()
   }
   message("Step 1: Extract the trees.", appendLF = FALSE)
   walking_dots()
@@ -14,18 +15,17 @@ test_adaboost2 <- function(H, data, input_checks = TRUE) {
   message("Step 2: Extract the model weights.", appendLF = FALSE)
   walking_dots()
   a <- vapply(H, "[[", numeric(1), "a") ; check_length(a)
-  message("Step 3: Make predictions/retrodictions.", appendLF = FALSE)
-  walking_dots()
+  message("Step 3: Make predictions/retrodictions.")
   pb <- txtProgressBar(min = 0, max = length(h), style = 3)
   y12_stumps <- sapply(seq_along(h), function(t) {
     setTxtProgressBar(pb, t)
     predict(h[[t]], newdata = data, type = "vector")
   })
   close(pb)  # Close progress bar when done
-  message("Step 4: Combine predictions/retrodictions.")
+  message("Step 4: Combine predictions/retrodictions.", appendLF = FALSE)
   ypred_stumps <- 2 * y12_stumps - 3
   ypred_ada <- sign(a %*% t(ypred_stumps))
-  # Completion message
+  walking_dots()
   message("Test process successfully completed.\n")
   ypred_ada
 }
