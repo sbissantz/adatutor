328feb7a78fcb42c7640e3d1063d2e5e20959bad
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2025-01-17 18:13:00 +0100

    Fix name

--- data-raw/altmext.R
+++ data-raw/altmext.R
@@ -10,21 +10,21 @@ data("ssrp723")
 
 # Variables of interest
 voi <- c("eid", "pid", "power.o", "effect_size.o", "n.o", "p_value.o",
-         "p_value.o", "replicated")
+         "p_value.o", "replicate")
 
 # Bind the data set with the pre-selected variables
 altmext723 <- rbind.data.frame( altmejd723[,colnames(altmejd723) %in% voi],
                                 ssrp723[,colnames(ssrp723) %in% voi] )
 
 # Make the outcome a factor and put it on the 1/2 scale (instead of 0/1)
-altmext723$replicated <- factor(altmext723$replicated + 1, # rpart consistency
+altmext723$replicate <- factor(altmext723$replicate + 1, # rpart consistency
                                 labels = c("failure", "success"))
 
 # Sort the data set by eid
-altmext <- altmext723[order(altmext723$eid),]
+altmejd <- altmext723[order(altmext723$eid),]
 
 # Add rownames
-rownames(altmext) <- paste0(seq(nrow(altmext)))
+rownames(altmejd) <- paste0(seq(nrow(altmext)))
 
 # Save the data set as R data
-usethis::use_data(altmext, overwrite = TRUE)
+usethis::use_data(altmejd, overwrite = TRUE)
