2edd9fd5b5a6dec91250622b4bba4c9906c88f32
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2025-01-17 18:12:35 +0100

    Remove comments

--- R/test_adaboost.R
+++ R/test_adaboost.R
@@ -6,9 +6,6 @@ test_adaboost <- function(X, ensemble) {
     a <- tree[["learner_weight"]]
     # Get the object as integer
     y_pred <- predict(h, X, type = "vector")
-    # Transform from 1/2 coding to -1/1
-    # TODO: DANIEL: Shouldn't the next parameter be called "a" ?
-    # TODO: And why -1/+1 coding and not 0/1 coding?!
     y_num <- 2 * y_pred - 3
     # Eta: learning rate (contribution of each weak learner)
     pred <- pred + a * y_num
