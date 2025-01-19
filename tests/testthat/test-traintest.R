30d50025a3240bab482ac58767737d872a8c3054
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2025-01-19 13:55:27 +0100

    Create test-traintest.R

---
+++ tests/testthat/test-traintest.R
@@ -0,0 +1,12 @@
+test_that("trainAda() and testAda() work", {
+  fit <- trainAda(Species ~ ., iris, T = 10, eta = 1,
+                  treehypar = rpart::rpart.control(maxdepth = 1, cp = 0),
+                  verbose = FALSE, input_checks = FALSE)
+  expect_length(fit, 10)
+  expect_equal(round(fit$t1$a, 1), 0.3)
+  expect_equal(round(fit$t10$a, 1), 0.5)
+  expect_match(class(fit$t1$h), "rpart")
+  expect_equal(attr(fit, "train"), as.name("iris"))
+  ypred <- testAda(fit, iris, input_checks = FALSE, verbose = FALSE)
+  expect_equal(ypred[1:6], rep(-1,6))
+})
